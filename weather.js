67b16c6d48ecf2735dcaa8bc16c6d7d6ac8a1314
Mihaly <mihaly_kisvarday@yahoo.com>
2017-04-05 14:51:35 +0100

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,13 +1,31 @@
-var myRequest = new XMLHttpRequest();
 
-    myRequest.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=9804f656d6029f92d0171355141aa791", true);
+var myRequest = new XMLHttpRequest();
     
-    myRequest.onload = function() {
-        
+    myRequest.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7df59206a9ff2b08a1c1e4ad01befdbf", true);
+    
+
+    myRequest.onreadystatechange = function() {
+       
         if (myRequest.readyState == 4 && myRequest.status == 200) {
             
             var weatherData = JSON.parse(myRequest.responseText);
+            
+            //Weather variables
+            var currentTemp = toCelsius(weatherData.main.temp).toFixed(2);
+            var humidity = weatherData.main.humidity;
+            
+            console.log(weatherData);
+            
         }
+        
+       
     }
-    
     myRequest.send();
+
+
+
+function toCelsius(kelvin) {
+    
+    var celsius = kelvin - 273.15;
+    return celsius;
+}
